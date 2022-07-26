d0cd7604a092130d0e0f942fc674d6ad64cf3007
Br15k <80359642+Br15k@users.noreply.github.com>
2022-07-26 15:29:13 -0600

    Add files via upload

---
+++ CanOfBread.py
@@ -0,0 +1,7 @@
+import discord
+import os
+from neuralintents import GenericAssistant
+
+client = discord.Client()
+
+client.run(MTAwMTU5NjMzNTgwOTYzMDIyOA.GShzpz.C2TlGgTw1oT0YCm5vMIV3qwCLclS89fa3fJImc)
